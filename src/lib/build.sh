589291a6faed183ad0685dd5b06d3e3e5fd91d27
Pavel Korolev <dev@borodust.org>
2021-11-04 14:54:28 +0300

    Add Android build

--- src/lib/build.sh
+++ src/lib/build.sh
@@ -2,19 +2,99 @@
 
 WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-BUILD_DIR="$WORK_DIR/build/"
+BUILD_TYPE="MinSizeRel"
 
-mkdir -p $BUILD_DIR && cd $BUILD_DIR
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel \
-      -DCMAKE_C_COMPILER=clang \
-      -DCMAKE_CXX_COMPILER=clang++ \
-      -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
-      -DCMAKE_SHARED_LINKER_FLAGS="-stdlib=libc++ -lc++abi" \
-      -DASSIMP_BUILD_ASSIMP_TOOLS=OFF \
-      -DASSIMP_BUILD_TESTS=OFF \
-      -DBUILD_DOCS=OFF \
-      -DINJECT_DEBUG_POSTFIX=OFF \
-      -DASSIMP_BUILD_ZLIB=ON \
-      $WORK_DIR
-cmake --build .
-cp $BUILD_DIR/assimp/code/libassimp.so.5 $BUILD_DIR
+REST_ARGS=
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    --arch)
+        TARGET_ARCH="$2"
+        shift
+        shift
+        ;;
+    --ndk)
+        NDK="$2"
+        shift
+        shift
+        ;;
+    --debug)
+        BUILD_TYPE="Debug"
+        shift
+        ;;
+    *)
+        REST_ARGS+="$1"
+        shift
+        ;;
+esac
+done
+
+BUILD_DIR="$WORK_DIR/build/$REST_ARGS/"
+
+function build_android {
+    if [[ -z "$NDK" ]]; then
+        echo "Path to Android NDK must be provided via --android-ndk"
+        exit 1
+    fi
+
+    ANDROID_ABI=arm64-v8a
+    case "$TARGET_ARCH" in
+        aarch64)
+            ANDROID_ABI=arm64-v8a
+            ;;
+        armv7a)
+            ANDROID_ABI=armeabi-v7a
+            ;;
+        *)
+            echo "Using ABI $ANDROID_ABI"
+            ;;
+    esac
+
+    mkdir -p $BUILD_DIR && cd $BUILD_DIR
+    cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+          -DANDROID_ABI=$ANDROID_ABI \
+          -DANDROID_PLATFORM=23 \
+          -DANDROID_ARM_NEON=ON \
+          -DCMAKE_TOOLCHAIN_FILE="$NDK/build/cmake/android.toolchain.cmake" \
+          -DASSIMP_BUILD_ASSIMP_TOOLS=OFF \
+          -DASSIMP_BUILD_TESTS=OFF \
+          -DBUILD_DOCS=OFF \
+          -DINJECT_DEBUG_POSTFIX=OFF \
+          -DASSIMP_BUILD_ZLIB=ON \
+          $WORK_DIR
+    cmake --build . --config "$BUILD_TYPE"
+    cp $BUILD_DIR/assimp/code/libassimp.so $BUILD_DIR
+}
+
+
+function build_desktop {
+    mkdir -p $BUILD_DIR && cd $BUILD_DIR
+    cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+          -DCMAKE_C_COMPILER=clang \
+          -DCMAKE_CXX_COMPILER=clang++ \
+          -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
+          -DCMAKE_SHARED_LINKER_FLAGS="-stdlib=libc++ -lc++abi" \
+          -DASSIMP_BUILD_ASSIMP_TOOLS=OFF \
+          -DASSIMP_BUILD_TESTS=OFF \
+          -DBUILD_DOCS=OFF \
+          -DINJECT_DEBUG_POSTFIX=OFF \
+          -DASSIMP_BUILD_ZLIB=ON \
+          $WORK_DIR
+    cmake --build . --config $BUILD_TYPE
+    cp $BUILD_DIR/assimp/code/libassimp.so.5 $BUILD_DIR
+}
+
+case "$REST_ARGS" in
+    desktop)
+        build_desktop
+        ;;
+    android)
+        build_android
+        ;;
+    *)
+        echo "Unrecognized platform $REST_ARGS"
+        exit -1
+        ;;
+esac
